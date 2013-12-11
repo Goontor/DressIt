582694c5abc1e42cea8a01ea60bbef3594848f0a
ninggu <ninggu@172.16.238.198>
2013-12-11 16:34:06 +0100

    jj

--- DressIt/src/com/dressit/MainActivity.java
+++ DressIt/src/com/dressit/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends Activity {
 	private User loggedUser;
 	DressItApplication myapp;
 	@Override
+	//hhhhh
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
